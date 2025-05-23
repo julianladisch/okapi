e35fbf0aab0cfc125075a9d1e9884fd86491ec26
julianladisch <julianladisch@users.noreply.github.com>
2025-05-23 12:48:06 +0200

    server and compose

--- okapi-common/src/test/java/org/folio/okapi/common/PauseResumeTest.java
+++ okapi-common/src/test/java/org/folio/okapi/common/PauseResumeTest.java
@@ -20,11 +20,10 @@ public class PauseResumeTest {
   public void test4(TestContext context) {
     Vertx vertx = Vertx.vertx();
 
-    //vertx.createHttpServer()
-    //  .requestHandler(x -> {})
-    //  .listen(0)
-    //  .compose(x -> vertx.createHttpClient().request(HttpMethod.GET, "example.invalid", "/"))
-    vertx.createHttpClient().request(HttpMethod.GET, "example.invalid", "/")
+    vertx.createHttpServer()
+      .requestHandler(x -> {})
+      .listen(0)
+      .compose(x -> vertx.createHttpClient().request(HttpMethod.GET, "example.invalid", "/"))
       .onComplete(context.asyncAssertFailure(res -> {
         context.assertTrue(res.getMessage().contains("example.invalid"), res.getMessage());
       }));
