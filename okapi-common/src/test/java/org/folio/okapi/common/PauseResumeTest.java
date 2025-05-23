c48c8724731a21cd16962c150a4e1af7a77b57bb
julianladisch <julianladisch@users.noreply.github.com>
2025-05-23 11:54:04 +0200

    create server in test4

--- okapi-common/src/test/java/org/folio/okapi/common/PauseResumeTest.java
+++ okapi-common/src/test/java/org/folio/okapi/common/PauseResumeTest.java
@@ -18,26 +18,20 @@ import org.junit.runner.RunWith;
 @RunWith(VertxUnitRunner.class)
 public class PauseResumeTest {
 
-  private Vertx vertx;
-  private static final int PORT = 9230;
+  @Test
+  public void test4(TestContext context) {
+    Vertx vertx = Vertx.vertx();
 
-  @Before
-  public void setUp(TestContext context) {
-    vertx = Vertx.vertx();
-    /*
     vertx.createHttpServer()
       .requestHandler(x -> {})
       .listen(0)
-      .onComplete(context.asyncAssertSuccess());
-    */
-  }
-
-  @Test
-  public void test4(TestContext context) {
-    HttpClient cli = vertx.createHttpClient();
-    cli.request(HttpMethod.POST, PORT, "localhostxxx", "/test2").onComplete(context.asyncAssertFailure(res -> {
-          context.assertTrue(res.getMessage().contains("localhostxxx"), res.getMessage());
-            }));
+      .compose(x -> {
+            HttpClient cli = vertx.createHttpClient();
+            return cli.request(HttpMethod.POST, PORT, "localhostxxx", "/test2")
+      })
+      .onComplete(context.asyncAssertFailure(res -> {
+        context.assertTrue(res.getMessage().contains("localhostxxx"), res.getMessage());
+      }));
   }
 
 }
