c15f975b09e311f1be6ef88a09ab9e00c76e0321
julianladisch <julianladisch@users.noreply.github.com>
2025-05-23 12:38:55 +0200

    255.255.255.255

--- okapi-common/src/test/java/org/folio/okapi/common/PauseResumeTest.java
+++ okapi-common/src/test/java/org/folio/okapi/common/PauseResumeTest.java
@@ -33,8 +33,8 @@ public class PauseResumeTest {
   @Test
   public void test4(TestContext context) {
     HttpClient cli = vertx.createHttpClient();
-    cli.request(HttpMethod.GET, "example.invalid", "/").onComplete(context.asyncAssertFailure(res -> {
-      context.assertTrue(res.getMessage().contains("example.invalid"), res.getMessage());
+    cli.request(HttpMethod.GET, "255.255.255.255", "/").onComplete(context.asyncAssertFailure(res -> {
+      context.assertTrue(res.getMessage().contains("255.255.255.255"), res.getMessage());
     }));
   }
 
